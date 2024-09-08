58649c0b98b724df5f41324ab3c8398bced56759
Master Yaj <12029220@stanleycollege@edu.au>
2024-09-08 14:51:05 +0800

    uwu

--- Test.py
+++ Test.py
@@ -1,10 +1,23 @@
+#Q1
 a = 15
 b = 12
-
 print(type(a))
 print(type(b))
-
-print(a+b)
-print(a-b)
-print(a*b)
-print(a/b)
+#Q2
+print('a + b =',a+b)
+print('a - b =',a-b)
+print('a * b =',a*b)
+print('a / b =',a/b)
+#Q3
+c = int(a/b)
+print('C is', c)
+c = float(a/b)
+print('c in float is' ,c)
+#Q4
+message = 'The result of a divided by b is'
+print(message + str(c))
+#Q5
+if a>b:
+    print('True')
+elif a==b:
+    print('False')
